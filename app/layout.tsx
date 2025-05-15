3ab04ea0c0ab590e01ae92832cb4f3478723621e
EugeneCoru <eugene@MacBook-Air-Eugene.local>
2025-05-15 13:43:57 +0300

    update font and bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono , Inter,Montserrat} from "next/font/google";
+import {  Inter,Montserrat} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const inter = Inter({
   subsets: ['latin', 'cyrillic'],
